03d647fa07684c1c2acb366530add01a4eae3b95
Tomasz Krakowiak <tomasz@taskablehq.com>
2020-10-11 15:01:42 +0200

    fixing folders

---
+++ app/Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using AzureStaticWebApps.Blazor.Authentication;
+
+namespace BlazorLogin
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services
+                .AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+                .AddStaticWebAppsAuthentication();
+            await builder.Build().RunAsync();
+        }
+    }
+}
